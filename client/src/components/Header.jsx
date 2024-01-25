74e0f49b8deb3878c3219cdce1e232a52a94cb9e
Aflal27 <130915738+Aflal27@users.noreply.github.com>
2024-01-25 13:45:13 +0530

    create header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoIosMoon } from "react-icons/io";
+
+export default function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar>
+      <Link
+        to={"/"}
+        className="text-sm sm:text-xl whitespace-nowrap font-semibold dark:text-white">
+        <span
+          className=" bg-gradient-to-r from-indigo-500 via-purple-500
+         to-pink-500 rounded-lg text-white px-2 py-1">
+          AFLAL
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={IoSearchOutline}
+          className=" hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden " color="gray" pill>
+        <IoSearchOutline />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 lg:hidden " color="gray" pill>
+          <IoIosMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/project"} as={"div"}>
+          <Link to="/project">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      Projects
+      <h1>projects</h1>
+    </div>
+  );
+}
