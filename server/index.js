78e72e57e6fa236bf72f405e9d373a1f546a0292
Aflal27 <130915738+Aflal27@users.noreply.github.com>
2024-01-25 14:14:19 +0530

    conect db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
+// db connect
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log("db error", err);
+  });
 app.listen(8000, () => {
   console.log("server is start");
 });
